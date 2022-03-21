338f85b307e05f9370aafe15172d675eed7e14a2
pokepup <s213012@dtu.dk>
2022-03-21 09:54:47 +0000

    Updates

---
+++ R/function_five.R
@@ -0,0 +1,27 @@
+#' Occurence of amino acids in a peptide
+#'
+#' @param aa_count
+#'
+#' @return
+#' @export
+#'
+#' @importFrom magrittr %>%
+aa_count <- function(amino_acid){
+  AA <- amino_acid %>%
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) %>%
+    as.character() %>%
+    unique()
+
+  counts <- sapply(AA, function(occurrence) stringr::str_count(string = amino_acid, pattern =  occurrence)) %>%
+    as.data.frame()
+
+  colnames(counts) <- c("Counts")
+  counts[["Name_me2"]] <- rownames(counts)
+
+  name_me4 <- counts %>%
+    ggplot2::ggplot(ggplot2::aes(x = amino_acid, y = Counts, fill = amino_acid)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw()
+
+  return(occurrence)
+}
